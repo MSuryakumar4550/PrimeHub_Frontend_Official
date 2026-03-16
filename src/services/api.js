6a8fad5bc4d9bd1a76eec73b9bfc2fba2af21c3d
M Suryakumar <muvsuryakumar@gmail.com>
2026-03-16 11:44:23 +0530

    Update API URL to Render cloud

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // backend base
+  baseURL: "https://prime-hub-student-management-system-for-onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
