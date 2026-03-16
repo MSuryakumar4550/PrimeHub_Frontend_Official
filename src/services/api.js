f2e97084bc24b84e5fe6190bfa6ded6992b82581
MSuryakumar4550 <muvsuryakumar@gmail.com>
2026-03-16 22:21:29 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://prime-hub-student-management-system-for.onrender.com/",
+   baseURL: "https://prime-hub-student-management-system-for.onrender.com/api",
   //  baseURL: "http:localhost:8080/api",
   headers: {
     "Content-Type": "application/json"
